deb5711aedafd0656f1e6ae85fd18be9e2634195
Ditiz <mathieu.25565@gmail.com>
2023-10-08 00:34:25 +0200

    feat: clean youtube watch url

--- features/MusicQuizz/playlist-input.tsx
+++ features/MusicQuizz/playlist-input.tsx
@@ -9,6 +9,16 @@ import { X } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
+function cleanYoutubeVideoUrl(url: string) {
+  const startIndex = url.indexOf("list=");
+
+  if (startIndex === -1) return url;
+
+  const endIndex = url.indexOf("&", startIndex);
+
+  return url.substring(startIndex + 5, endIndex);
+}
+
 const PlaylistInput = () => {
   const [input, setInput] = useState("");
   const [errors, setErrors] = useAtom(errorsAtom);
@@ -19,11 +29,14 @@ const PlaylistInput = () => {
     let playlistId = input.trim();
 
     // clean url
-    if (playlistId.includes("https://www.youtube.com/playlist?list=")) {
+    if (playlistId.includes("https://www.youtube.com/playlist")) {
       playlistId = playlistId.replace(
         "https://www.youtube.com/playlist?list=",
         ""
       );
+    } else if (playlistId.startsWith("https://www.youtube.com/watch")) {
+      console.log(cleanYoutubeVideoUrl(playlistId));
+      playlistId = cleanYoutubeVideoUrl(playlistId)! ?? playlistId;
     }
 
     if (!playlistId) return;
