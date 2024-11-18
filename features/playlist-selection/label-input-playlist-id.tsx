dd3644dd8086b3afbdb2b7d5a3537e1896a4a1a1
Mathieu <mathieu.25565@gmail.com>
2024-11-18 10:36:11 +0100

    Update playlist-input.tsx

--- features/playlist-selection/label-input-playlist-id.tsx
+++ features/playlist-selection/label-input-playlist-id.tsx
@@ -20,14 +20,14 @@ const LabelInputPlaylistId = () => {
 					<h2>How to Get a YouTube Playlist ID</h2>
 					<p>1. Open the playlist on YouTube.</p>
 					<p>
-						2. Look at the URL, e.g.,{" "}
+						2. Look at the URL, e.g.,
 						<code>
-							https://www.youtube.com/playlist?list=PL9tY0BWXOZFta3_NWugU1o72a6Ae-bD7V
+							https://www.youtube.com/playlist?list=PLG7EWwhWrY25Q7RiWjWB20aiyPszZMlk5
 						</code>
 					</p>
 					<p>
 						3. The ID is the part after <code>list=</code>, here:{" "}
-						<strong>PL9tY0BWXOZFta3_NWugU1o72a6Ae</strong>
+						<strong>PLG7EWwhWrY25Q7RiWjWB20aiyPszZMlk5</strong>
 					</p>
 				</TooltipContent>
 			</Tooltip>
