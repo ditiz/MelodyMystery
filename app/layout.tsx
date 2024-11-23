3afef667cc0e1500cf9ae9bdfa85424af1919d20
ditiz <mathieu.25565@gmail.com>
2024-11-23 13:27:19 +0100

    feat: animate gradient background

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 					disableTransitionOnChange
 				>
 					<div className="relative h-full w-full dark:md:bg-slate-950">
-						<div className="absolute top-[-75%] left-[20%] h-[1000px] w-[1000px] rounded-full blur-[150px] opacity-50 bg-conic-gradient" />
+						<div className="absolute top-[-75%] left-[20%] h-[1000px] w-[1000px] rounded-full blur-[150px] opacity-50 bg-conic-gradient animate-spin-slow" />
 						<header className="relative">
 							<nav>
 								<ul className="w-full flex items-center justify-around p-2">
